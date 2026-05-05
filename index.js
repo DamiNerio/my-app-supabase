2f12c7cdf2d9664c1b26bc696843a56bf4773de6
Cesar Damian Nerio Gonzalez <damian.cng@hotmail.com>
2026-05-05 10:56:12 -0600

    Primer commit

---
+++ index.js
@@ -0,0 +1,17 @@
+import { createClient } from '@supabase/supabase-js'
+const supabaseUrl = 'https://vyejudhgyrlqkcljjaaq.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ5ZWp1ZGhneXJscWtjbGpqYWFxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc5MTk5OTUsImV4cCI6MjA5MzQ5NTk5NX0.lwn8hpQddi5IDunE_ut81b-hagVcyJgjv__3qtcEfto'
+const supabase = createClient(supabaseUrl, supabaseKey)
+async function obtenerEstudiantes() {
+ const { data, error } = await supabase
+ .from('estudiantes')
+ .select('*')
+ if (error) {
+ console.log('Error:', error)
+ } else {
+ console.log('Datos:', data)
+ }
+}
+
+
+obtenerEstudiantes()
